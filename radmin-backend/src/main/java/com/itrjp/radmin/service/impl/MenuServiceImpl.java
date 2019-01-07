fc1c50472ecbd6db3e41f10e8953d24095168bb7
天空里熬翔 <979668507@qq.com>
2019-01-07 18:05:27 +0800

    查询子菜单

--- radmin-backend/src/main/java/com/itrjp/radmin/service/impl/MenuServiceImpl.java
+++ radmin-backend/src/main/java/com/itrjp/radmin/service/impl/MenuServiceImpl.java
@@ -3,6 +3,7 @@ package com.itrjp.radmin.service.impl;
 import com.itrjp.radmin.bean.Menu;
 import com.itrjp.radmin.dao.MenuMapper;
 import com.itrjp.radmin.service.MenuService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,44 +23,56 @@ public class MenuServiceImpl implements MenuService {
 
     @Override
     public List<Menu> getAll() {
-        List<Menu> menus = menuMapper.selectAll();
-
+        //原始数据
+        List<Menu> rootMenus = menuMapper.selectAll();
         //子菜单
-        List<Menu> children = menus.stream()
+        List<Menu> children = rootMenus.stream()
                 .filter((menu) -> menu.getActiveFlag() == 1 && menu.getParentId() != null)
-                .collect(Collectors.toList());
-
-        List<Menu> collect = menus.stream()
-                .filter((menu) -> menu.getActiveFlag() == 1 && menu.getParentId() == null)
-                .filter(menu -> {
-
-                    return true;
-                })
                 .sorted(Comparator.comparing(Menu::getOrderNum))
-
                 .collect(Collectors.toList());
-
-
+        //最终数据
+        List<Menu> collect = rootMenus.stream()
+                .filter((menu) -> menu.getActiveFlag() == 1 && menu.getParentId() == null)
+                .sorted(Comparator.comparing(Menu::getOrderNum))
+                .collect(Collectors.toList());
+        //查找子菜单
         for (Menu menu : collect) {
-            for (Menu child : children) {
-
-                if (menu.getId().equals(child.getParentId())) {
-                    List<Menu> childrenList = menu.getChildren();
-                    if (childrenList == null) {
-                        childrenList = new ArrayList<>();
-                    }
-                    childrenList.add(child);
-                    menu.setChildren(childrenList);
-                }
-            }
+            menu.setChildren(getChild(menu.getId(), children));
         }
-
 
         return collect;
     }
 
 
-    private void setChildren() {
+    /**
+     * 递归查找子菜单
+     *
+     * @param id       id
+     * @param rootMenu 要查找的菜单
+     * @return
+     */
+    private List<Menu> getChild(String id, List<Menu> rootMenu) {
 
+        // 子菜单
+        List<Menu> childList = new ArrayList<>();
+        for (Menu menu : rootMenu) {
+            // 遍历所有节点，将父菜单id与传过来的id比较
+            if (StringUtils.isNotBlank(menu.getParentId())) {
+                if (menu.getParentId().equals(id)) {
+                    childList.add(menu);
+                }
+            }
+        }
+        // 把子菜单的子菜单再循环一遍
+        for (Menu menu : childList) {// 没有url子菜单还有子菜单
+
+            // 递归
+            menu.setChildren(getChild(menu.getId(), rootMenu));
+
+        } // 递归退出条件
+        if (childList.size() == 0) {
+            return null;
+        }
+        return childList.stream().sorted(Comparator.comparing(Menu::getOrderNum)).collect(Collectors.toList());
     }
 }
