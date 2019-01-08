b66d5aea04da9d96fe1eae2bfc226e2ae524d895
天空里熬翔 <979668507@qq.com>
2019-01-08 17:07:33 +0800

    简化代码

--- radmin-backend/src/main/java/com/itrjp/radmin/service/impl/MenuServiceImpl.java
+++ radmin-backend/src/main/java/com/itrjp/radmin/service/impl/MenuServiceImpl.java
@@ -3,11 +3,9 @@ package com.itrjp.radmin.service.impl;
 import com.itrjp.radmin.bean.Menu;
 import com.itrjp.radmin.dao.MenuMapper;
 import com.itrjp.radmin.service.MenuService;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,16 +29,15 @@ public class MenuServiceImpl implements MenuService {
                 .sorted(Comparator.comparing(Menu::getOrderNum))
                 .collect(Collectors.toList());
         //最终数据
-        List<Menu> collect = rootMenus.stream()
+        List<Menu> menus = rootMenus.stream()
                 .filter((menu) -> menu.getActiveFlag() == 1 && menu.getParentId() == null)
+                .map(menu -> {
+                    menu.setChildren(getChild(menu.getId(), children));
+                    return menu;
+                })
                 .sorted(Comparator.comparing(Menu::getOrderNum))
                 .collect(Collectors.toList());
-        //查找子菜单
-        for (Menu menu : collect) {
-            menu.setChildren(getChild(menu.getId(), children));
-        }
-
-        return collect;
+        return menus;
     }
 
 
@@ -54,25 +51,19 @@ public class MenuServiceImpl implements MenuService {
     private List<Menu> getChild(String id, List<Menu> rootMenu) {
 
         // 子菜单
-        List<Menu> childList = new ArrayList<>();
-        for (Menu menu : rootMenu) {
-            // 遍历所有节点，将父菜单id与传过来的id比较
-            if (StringUtils.isNotBlank(menu.getParentId())) {
-                if (menu.getParentId().equals(id)) {
-                    childList.add(menu);
-                }
-            }
-        }
-        // 把子菜单的子菜单再循环一遍
-        for (Menu menu : childList) {// 没有url子菜单还有子菜单
+        List<Menu> childList = rootMenu.stream()
 
-            // 递归
-            menu.setChildren(getChild(menu.getId(), rootMenu));
-
-        } // 递归退出条件
+                .filter(menu -> menu.getParentId().equals(id))
+                .map(menu -> {
+                    menu.setChildren(getChild(menu.getId(), rootMenu));
+                    return menu;
+                })
+                .sorted(Comparator.comparing(Menu::getOrderNum))
+                .collect(Collectors.toList());
+        // 递归退出条件
         if (childList.size() == 0) {
             return null;
         }
-        return childList.stream().sorted(Comparator.comparing(Menu::getOrderNum)).collect(Collectors.toList());
+        return childList;
     }
 }
