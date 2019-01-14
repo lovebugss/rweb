aed27ba70fbc62cfc5d79c88f58f3e3cd89dfc1c
天空里熬翔 <979668507@qq.com>
2019-01-14 17:07:40 +0800

    优化代码

--- radmin-forntend/src/index.js
+++ radmin-forntend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {AppContainer} from 'react-hot-loader';
 import configureStore, {history} from './configureStore';
 import {ConnectedRouter} from 'connected-react-router'
 import "./index.css"
-const store = configureStore();
 
+const store = configureStore();
 ReactDOM.render(
     <AppContainer>
         <Provider store={store}>
@@ -16,9 +16,6 @@ ReactDOM.render(
                 <App />
             </ConnectedRouter>
         </Provider>
-    </AppContainer>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+    </AppContainer>,
+    document.getElementById('root'));
 serviceWorker.unregister();
