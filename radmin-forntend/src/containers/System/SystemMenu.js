a737a8a0ee9a1de7c465b96132717ff18f0054bb
天空里熬翔 <979668507@qq.com>
2019-01-22 17:35:42 +0800

    菜单管理

--- radmin-forntend/src/containers/System/SystemMenu.js
+++ radmin-forntend/src/containers/System/SystemMenu.js
@@ -5,8 +5,12 @@
  * Created by renjp on 2019/1/9.
  */
 import React from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {actions} from '../../reducers/menu';
 import {Breadcrumb, Form, Icon, Layout} from "antd";
 import Content from '../../components/Content';
+import MenuTable from './MenuTable'
 
 class SystemMenu extends React.Component {
 
@@ -15,14 +19,27 @@ class SystemMenu extends React.Component {
     }
 
     render() {
+
+        const {data} = this.props;
         return (
             <Content
-                navList={["aaa","bb","cc"]}
+                navList={["aaa", "bb", "cc"]}
             >
-
-
+                <MenuTable
+                    data={data}
+                />
             </Content>
         );
     }
 }
-export default SystemMenu;
+function mapStateToProps(state) {
+    return {
+        data: state.menu.menuData,
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return {
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(SystemMenu);
